8e1263fb90def05ae83f1e9b8249cf7800e3d509
rodel <rodelagbing@gmail.com>
2022-06-05 20:43:51 -0400

    Initial commit

---
+++ environment/clo835lab1/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Rodel"
+    "App"   = "Web"
+    "Project" = "CLO835-Assignment1"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "catsandogs"
+}
